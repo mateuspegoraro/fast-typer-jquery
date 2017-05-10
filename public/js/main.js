c15c8f9f39c4171b40b62fde2ff0d02b98f4b813
Mateus <mateus.pegoraro@perceptvision.com.br>
2017-05-10 17:36:13 -0300

    primeiro commit

---
+++ public/js/main.js
@@ -0,0 +1,68 @@
+var campo = $(".campo-digitacao");
+var tempoInicial = $("#tempo-digitacao").text();
+
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
+
+
+function atualizaTamanhoFrase() {
+
+    var frase = $(".frase").text();
+    var numPalavras  = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+
+    campo.on("input", function() {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+
+    });
+}
+
+function inicializaCronometro() {
+
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+                //campo.addClass("campo-desativado");
+                campo.toggleClass("campo-desativado");//on/off
+            }
+        }, 1000);
+    });
+}
+
+$("#botao-reiniciar").click(reiniciaJogo);
+
+var frase = $(".frase").text();
+campo.on("input", function(){
+  
+});
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+
+    $("#tempo-digitacao").text(tempoInicial);
+    campo.removeClass("campo-desativado");
+    //campo.toggleClass("campo-desativado");//on/off
+    inicializaCronometro();
+}
